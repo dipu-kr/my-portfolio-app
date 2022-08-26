ec536cf97b58720cfdb48950faae329183cdac7c
dipu-kr <dipum906@gmail.com>
2022-08-26 12:15:56 +0530

    third commit

--- src/components/About.js
+++ src/components/About.js
@@ -2,17 +2,22 @@ import React from "react";
 import "../css/about.css";
 import { AiFillLinkedin } from "react-icons/ai";
 import { BsGithub, BsWhatsapp } from "react-icons/bs";
+import { BiPhoneCall } from "react-icons/bi";
 
 const About = () => {
   return (
     <section className="about-main" id="about">
       <div className="about-name-div">
-        <h1>Hi, My name is Dipu Kumar</h1>
+        <h1>Hi, I'm Dipu Kumar</h1>
       </div>
       <div className="about-para-div">
         <p>A front-end developer with a passion for learning and creating.</p>
       </div>
       <div className="about-icon-div">
+        <a href="tel:7644949363" target="_blank" rel="noreferrer noopener">
+          <BiPhoneCall className="about-icon" />
+        </a>
+
         <a
           href="https://linkedin.com/in/dipu-kr"
           target="_blank"
